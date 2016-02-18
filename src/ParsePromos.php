0e568e7ebcf32d1267675d98f01c2d9e1194c814
Nick DeNardis <nick.denardis@gmail.com>
2016-02-18 13:17:33 -0500

    PSR-2 fixes

--- src/ParsePromos.php
+++ src/ParsePromos.php
@@ -52,12 +52,13 @@ class ParsePromos implements ParserInterface
      * @param array $group_reference
      * @return array
      */
-    public function groups(array $promos, array $group_reference = array()) {
+    public function groups(array $promos, array $group_reference = array())
+    {
         $groups = array();
 
         // Make sure we have promotional items
-        if(is_array($promos)) {
-            foreach($promos as $items) {
+        if (is_array($promos)) {
+            foreach ($promos as $items) {
                 // Get the first item in that group
                 $first_item = current($items);
 
